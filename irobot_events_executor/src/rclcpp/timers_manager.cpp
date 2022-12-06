93e58873d02cd3b41f7459f0fcbbf1487807b9eb
Mauro Passerino <mpasserino@irobot.com>
2022-12-06 19:12:55 -0300

    Fix timers-manager segfault

--- irobot_events_executor/src/rclcpp/timers_manager.cpp
+++ irobot_events_executor/src/rclcpp/timers_manager.cpp
@@ -70,11 +70,7 @@ void TimersManager::stop()
 {
   // Lock stop() function to prevent race condition in destructor
   std::unique_lock<std::mutex> lock(stop_mutex_);
-  // Nothing to do if the timers thread is not running
-  // or if another thread already signaled to stop.
-  if (!running_.exchange(false)) {
-    return;
-  }
+  running_ = false;
 
   // Notify the timers manager thread to wake up
   {
