2fa3b4de02874ce30b15a8ac7b88927fc0ade29f
Josh Finken <jfinken@irobot.com>
2024-02-16 10:24:50 -0700

    Temporarily comment-out the lock-free-events-queue example

--- events_executor_examples/src/lock_free_queue.cpp
+++ events_executor_examples/src/lock_free_queue.cpp
@@ -5,24 +5,28 @@
 #include "irobot_lock_free_events_queue/lock_free_events_queue.hpp"
 
 #include "rclcpp/executors/events_executor/events_executor.hpp"
+//#include "rclcpp/experimental/executors/events_executor/events_executor.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 #include "example_nodes.hpp"
 
 int main(int argc, char * argv[])
 {
+  /*
   rclcpp::init(argc, argv);
 
   auto publisher_node = std::make_shared<MinimalPublisher>();
   auto subscriber_node = std::make_shared<MinimalSubscriber>();
 
   auto lock_free_queue = std::make_unique<LockFreeEventsQueue>();
-  auto executor = std::make_shared<rclcpp::executors::EventsExecutor>(std::move(lock_free_queue));
+  // auto executor = std::make_shared<rclcpp::executors::EventsExecutor>(std::move(lock_free_queue));
+  auto executor = std::make_shared<rclcpp::experimental::executors::EventsExecutor>(std::move(lock_free_queue));
 
   executor->add_node(publisher_node);
   executor->add_node(subscriber_node);
   executor->spin();
 
   rclcpp::shutdown();
+  */
   return 0;
 }
